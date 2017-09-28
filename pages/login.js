f60374778de3c73c0309cedcbc0ab5e1cbad330d
Luis Gerardo <luis.encinas1506@gmail.com>
2017-09-27 18:24:30 -0700

    Logo UI Added

--- pages/login.js
+++ pages/login.js
@@ -1,156 +1,235 @@
-import react from 'react'
-import {Row, Col, Container} from 'react-grid-system'
+import { Component } from 'react'
+import {Row, Col, Container, Visible} from 'react-grid-system'
 import App from '../components/app/App'
+import Transition from 'react-motion-ui-pack'
+import LoginForm from '../components/login/loginForm.js'
 
-export default () => (
-  <App>
-    <Row className='Row'>
-      <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 50}}>
-        <div className='login-wrapper'>
-          <form>
-            <Container>
-              <Row>
-                <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 20}}>
-                  <input type='text' className='input__field input__field--kyo'/>
-                  <label className='input__label input__label--kyo'><span className='input__label-content input__label-content--kyo'>Nombre de la Ruta</span></label>
-                </Col>
-                <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 20}}>
-                  <input type='text' className='input__field input__field--kyo' />
-                  <label className='input__label input__label--kyo'><span className='input__label-content input__label-content--kyo'>URL de la imagen</span></label>
-                </Col>
-                <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 20}}>
-                  <button type="submit" form="form1" value="Submit">Submit</button>
+class Login extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      render: false,
+      top: '45%',
+      zIndex: 1,
+      opacity: 1
+    }
+  }
+
+  testing () {
+    if(this.state.render) {
+      return(
+        <LoginForm />
+      )
+    }
+    else {
+      return null
+    }
+  }
+
+  handleToggle () {
+    this.setState({
+      render: true,
+      top: '68%',
+      zIndex: -1,
+      opacity: 0
+    });
+  }
+
+  render () {
+    var prueba = {
+      top: this.state.top,
+      zIndex: this.state.zIndex,
+      opacity: this.state.opacity
+    };
+    return (
+      <div>
+        <Visible md lg xl>
+          <App>
+            <Transition
+              component={false}
+              enter={{
+                opacity: 1,
+                scale: 1
+              }}
+              leave={{
+                opacity: 0,
+                scale: 0
+              }}
+              >
+              <Row className='Row'>
+                <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 50}}>
+                  <div className='login-wrapper'>
+                    <form>
+                      <Container>
+                        <Row>
+                          <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 20}}>
+                            <input type='text' className='input__field input__field--kyo'/>
+                            <label className='input__label input__label--kyo'><span className='input__label-content input__label-content--kyo'>Nombre de la Ruta</span></label>
+                          </Col>
+                          <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 20}}>
+                            <input type='text' className='input__field input__field--kyo' />
+                            <label className='input__label input__label--kyo'><span className='input__label-content input__label-content--kyo'>URL de la imagen</span></label>
+                          </Col>
+                          <Col xs={12} sm={12} md={12} xl={12} style={{marginTop: 20}}>
+                            <button type='submit' form='form1' value='Submit'>Submit</button>
+                          </Col>
+                        </Row>
+                      </Container>
+                    </form>
+                  </div>
                 </Col>
               </Row>
-            </Container>
-          </form>
-        </div>
-      </Col>
-    </Row>
-    <style jsx>
-     {`
-        input {
-        position: relative;
-        z-index: 1;
-        display: inline-block;
-        margin: 1em;
-        max-width: 350px;
-        width: calc(100% - 2em);
-        vertical-align: top;
-        }
+            </Transition>
+            <style jsx>
+             {`
+                input {
+                position: relative;
+                z-index: 1;
+                display: inline-block;
+                margin: 1em;
+                max-width: 350px;
+                width: calc(100% - 2em);
+                vertical-align: top;
+                }
 
-        .input__field {
-        position: relative;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        padding: 0.8em;
-        width: 60%;
-        border: none;
-        border-radius: 0;
-        background: #f0f0f0;
-        color: #aaa;
-        font-weight: bold;
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        -webkit-appearance: none; /* for box shadows to show on iOS */
-        }
+                .input__field {
+                position: relative;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                border: none;
+                font-weight: bold;
+                font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                -webkit-appearance: none;
+                padding: 0.85em 1.5em;
+              	width: 85%;
+              	border-radius: 2em;
+              	background: #fff;
+              	color: #535d92;
+                }
 
-        .input__field:focus {
-        outline: none;
-        }
+                .input__field:focus {
+                outline: none;
+                }
 
-        .input__label {
-        display: inline-block;
-        padding: 0 1em;
-        width: 40%;
-        color: #6a7989;
-        font-weight: bold;
-        font-size: 70.25%;
-        -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-        -webkit-touch-callout: none;
-        -webkit-user-select: none;
-        -khtml-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        }
+                .input__label {
+                display: inline-block;
+                padding: 0 1em;
+                color: #6a7989;
+                font-weight: bold;
+                font-size: 70.25%;
+                z-index: 0;
+              	width: 380px;
+              	text-align: left;
+                font-size: 16px;
+                margin-left: auto;
+                margin-right: auto;
+                display: block;
+                }
 
-        .input__label-content {
-        position: relative;
-        display: block;
-        padding: 1.6em 0;
-        width: 100%;
-        }
-        input--kyo {
-      	z-index: auto;
-        }
-        .input__field--kyo {
-      	padding: 0.85em 1.5em;
-      	width: 85%;
-      	border-radius: 2em;
-      	background: #fff;
-      	color: #535d92;
-      }
+                .input__label-content {
+                position: relative;
+                display: block;
+                padding: 1.6em 0;
+                width: 100%;
+                }
 
-      .input__label--kyo {
-      	z-index: 0;
-      	width: 380px;
-      	text-align: left;
-        font-size: 16px;
-        margin-left: auto;
-        margin-right: auto;
-        display: block;
-      }
+              .login-wrapper {
+                width: 600px;
+                height: 350px;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                background: #E8E8E8;
+                border-radius: 50px;
+              }
 
-      .input__label--kyo::after {
-      	content: '';
-      	position: fixed;
-      	top: 0;
-      	left: 0;
-      	z-index: 1000;
-      	width: 100%;
-      	height: 100%;
-      	background: #ed3d47;
-      	opacity: 0;
-      	-webkit-transition: opacity 0.5s ease-in-out;
-      	transition: opacity 0.3s ease-in-out;
-      	pointer-events: none;
-      }
+              @media only screen and (max-width: 620px) {
+                .login-wrapper {
+                  width: auto;
+                }
+              }
+             `}
+            </style>
+          </App>
+        </Visible>
+        <Visible xs sm>
+          <div className='wrapper'>
+            <div className='img-wrapper'>
+              <img src='../static/logo.png' style={{width: 200, height: 200}} />
+            </div>
+            {this.testing()}
+            <button type='button' className='button' style={prueba} onClick={this.handleToggle.bind(this)}>Signed in</button>
+            <style jsx>
+             {`
+              .wrapper {
+                background-color:  #ed3d47;
+                width: 100%;
+                height: 100vh;
+                padding: 0;
+                margin: 0;
+              }
+              .img-wrapper {
+                margin-left: auto;
+                margin-right: auto;
+                display:block;
+                width: 200px;
+                height: 200px;
+                position: relative;
+                top: 40px;
+              }
+              .h1-wrapper {
+                position: absolute;
+                top: 8%;
+                left: 36%;
+                color: #F9F9F9;
+              }
+              .button {
+                background: #F9F9F9;
+                background-position: 0 200px;
+                height: 60px;
+                width: 150px;
+                margin: auto;
+                position: absolute;
+                left: 0; right: 0;
+                outline: none;
+                border: none;
+                border-radius: 28px;
+                font: 500 20px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                cursor: pointer;
+                color: #ed3d47;
+                opacity: 1;
+                z-index: 0;
+                -webkit-transition: top .5s, z-index 2s, opacity .5s;
+                transition: top .5s, z-index 2s, opacity .5s;
+              }
 
-      .input__label-content--kyo {
-      	padding: 0.5em 0;
-      }
+              .login-wrapper {
+                width: 600px;
+                height: 350px;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                background: #E8E8E8;
+                border-radius: 50px;
+              }
+              body{
+                margin: 0;
+                padding 0;
+              }
+              @media only screen and (max-width: 620px) {
+                .login-wrapper {
+                  width: auto;
+                }
+              }
+             `}
+            </style>
+          </div>
+        </Visible>
+      </div>
+    )
+  }
+}
 
-      .input__field--kyo:focus,
-      .input__field--kyo:focus +  .input__label--kyo .input__label-content--kyo {
-      	z-index: 10000;
-      }
-
-      .input__field--kyo:focus + .input__label--kyo {
-      	color: #fff;
-      }
-
-      .input__field--kyo:focus + .input__label--kyo::after {
-      	opacity: 1;
-      }
-
-      .login-wrapper {
-        width: 600px;
-        height: 350px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        background: #E8E8E8;
-        border-radius: 50px;
-      }
-
-      @media only screen and (max-width: 620px) {
-        .login-wrapper {
-          width: auto;
-        }
-      }
-     `}
-    </style>
-  </App>
-)
+export default Login
