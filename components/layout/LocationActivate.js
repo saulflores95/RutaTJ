03b480f5074d9c7ce6ed809a8d232ff049363c13
Luis Gerardo <luis.encinas1506@gmail.com>
2017-10-07 15:59:36 -0700

    Location modal added

--- components/layout/LocationActivate.js
+++ components/layout/LocationActivate.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import Transition from 'react-motion-ui-pack'
 import io from 'socket.io-client'
+import { Row, Col, Hidden } from 'react-grid-system'
 
 class LocationActivate extends Component {
   constructor (props) {
@@ -91,45 +92,104 @@ class LocationActivate extends Component {
     var drawer = {
       display: this.state.display
     }
+
     return (
-      <div>
-        <Transition
-          component={false}
-          enter={{
-            opacity: 1,
-            scale: 1
-          }}
-          leave={{
-            opacity: 0,
-            scale: 0
-          }}
-          >
-          <div className='wrapper' style={drawer}>
-            <p>Activa tu camion</p>
-            <input value={this.state.inputValue} placeholder='Nombre de usuario' onChange={evt => this.updateInputValue(evt)} />
-            <button type='button' className='button' onClick={this.getLocationUpdate.bind(this)}>Permitir</button>
-            <button type='button' className='button' onClick={this.handleClose.bind(this)}>Cancelar</button>
+      <div className='wrapper' style={drawer}>
+      <Transition
+        component={false}
+        enter={{
+          opacity: 1,
+          scale: 1
+        }}
+        leave={{
+          opacity: 0,
+          scale: 0
+        }}
+        >
+          <div className='modal'>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <p>Activa tu camion</p>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <input className='input' value={this.state.inputValue} placeholder='Nombre de usuario' onChange={evt => this.updateInputValue(evt)} />
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <div className='button-wrapper'>
+                  <button type='button' className='button' onClick={this.getLocationUpdate.bind(this)}>Permitir</button>
+                </div>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <div className='button-wrapper'>
+                  <button type='button' className='button' onClick={this.handleClose.bind(this)}>Cancelar</button>
+                </div>
+              </Col>
+            </Row>
           </div>
         </Transition>
         <style jsx>
           {`
-          .wrapper {
-            z-index: 1000;
-            background-color: #ffffff;
-            border-style: solid;
-            border-color: #ed3d47;
-            border-width: 5px;
-            width: 500px;
-            height: 400px;
+            p {
+              text-align: center;
+            }
+
+          .button-wrapper {
             margin-left: auto;
             margin-right: auto;
-            top: 12%;
+            display: block;
+            width: 50px;
+          }
+
+          .input {
+            display: inline-block;
+             background: #F9F9F9;
+             width: 90%;
+             margin-left: 30px;
+             margin-right: 30px;
+             border: 0;
+             border-bottom: 1px solid lightgrey;
+             padding: 12px 0;
+             outline: 0;
+             -webkit-transition: border-bottom 1s;
+             font-size: 16px;
+          }
+
+          .input:focus {
+              border-bottom: 1px solid red;
+          }
+
+          .wrapper {
+            z-index: 1000;
+            background-color: rgba(0,0,0,.8);
+            width: 100%;
+            height: 100%;
+            margin-left: auto;
+            margin-right: auto;
             left: 0;
             right: 0;
             position: absolute;
           }
-          @media only screen and (min-width: 300px) and (max-width: 500px){
-            .wrapper {
+          .modal {
+            z-index: 4000;
+            background-color: white;
+            border-radius: 5%;
+            width: 400px;
+            height: 250px;
+            margin-left: auto;
+            margin-right: auto;
+            left: 0;
+            right: 0;
+            position: absolute;
+            top: 28%;
+          }
+          .modal-wrapper {
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0,0,0,.8);
+          }
+
+          @media only screen and (max-width: 400px){
+            .modal {
               width: auto;
               height: 50%;
               margin-left: 20px;
